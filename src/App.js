adcb85b7ed21d9ce6c7fe5939ae33a9067fb1865
Slim Yelow <slimyelow@gmail.com>
2021-06-17 19:13:07 +0200

    add remove function and button

--- src/App.js
+++ src/App.js
@@ -5,7 +5,42 @@ import Tours from './Tours'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading, setLoading] = useState(false)
+  const [tours, setTours] = useState([])
+  
+  // remove tour function
+    const removeTour = (id) => {
+      const newTours = tours.filter((tour) => id !== tour.id)  
+      setTours(newTours);
+  } 
+
+  // go fetch my tours from api
+  const fetchTours = async() => {
+    setLoading(true)
+    try {          
+        const response = await fetch(url)
+        const tours = await response.json()
+        setLoading(false)
+        setTours(tours)
+        console.log(tours);
+    } catch (error) {
+      setLoading(false)    
+      console.log(error)
+    } 
+  }
+  // can't be inside a conditional!
+  useEffect(() => {
+    fetchTours()
+  },[])
+
+  if (loading) {
+    return <main>
+      <Loading />
+    </main>
+  }
+  return <main>
+    <Tours tours={tours} removeTour={removeTour}/>
+  </main>
 }
 
 export default App
--- src/Tour.js
+++ src/Tour.js
@@ -1,7 +1,24 @@
 import React, { useState } from 'react';
 
-const Tour = () => {
-  return <h2>tour component</h2>;
+
+ 
+const Tour = ({id, image, name, info, price, removeTour}) => {
+  // readmore toggle
+  const [readmore, setReadmore] = useState(false)
+
+  return <article className="single-tour">
+    <img src={image} alt={name} />
+    <footer>
+      <div className="tour-info">
+        <h4>{name}</h4>
+        <h4 className="tour-price">${price}</h4>
+      </div>
+      <p>{readmore ? info : info.substring(0, 200)} . . .  {/* nifty substring function */}
+        <button onClick={()=> setReadmore(!readmore)}>{readmore ? 'Show Less' : 'Show More'}</button>      
+      </p> 
+      <button className="delete-btn" onClick={()=> removeTour(id)} >Not Interested</button>
+    </footer>
+  </article>
 };
 
 export default Tour;
